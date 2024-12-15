c3dbdd4457c91804296ebb0698ac0be82f126f6d
Braz1er <80370609+Braz1er@users.noreply.github.com>
2024-12-16 00:39:28 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_TTT_FILE C:/Users/artem/CLionProjects/TTT/cmake-build-debug/TTT.exe)
-set(__QT_DEPLOY_TARGET_TTT_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_TTT_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
